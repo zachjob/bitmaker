d00cfaa9aa4a71522bfb054b1aba4b51c33ab16e
Julie Hache <elektrophile@gmail.com>
2014-07-16 11:18:15 -0400

    Add User model with Sorcery

---
+++ db/migrate/20140716143723_sorcery_core.rb
@@ -0,0 +1,17 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    drop_table :users
+
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email,            :null => false
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715155748) do
+ActiveRecord::Schema.define(version: 20140716143723) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -30,5 +30,17 @@ ActiveRecord::Schema.define(version: 20140715155748) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email",            null: false
+    t.string   "crypted_password", null: false
+    t.string   "salt",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
