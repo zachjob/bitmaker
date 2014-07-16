d5f75252c3ed71869b6ee224d4355e5d4c1bfdc5
Julie Hache <elektrophile@gmail.com>
2014-07-16 11:19:11 -0400

    Create UsersController, signup view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "projects#index"
 
   resources :projects, only: [:index, :new, :create]
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
