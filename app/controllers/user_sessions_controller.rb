dfd394c60165fc398051e9414ecc13f6dbab0a56
Julie Hache <elektrophile@gmail.com>
2014-07-16 11:33:54 -0400

    Add remember_me extension

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-     if @user = login(params[:email], params[:password])
+     if @user = login(params[:email], params[:password], params[:remember_me])
       redirect_back_or_to(root_url, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
