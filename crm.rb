4d2bf8c1054281c29ff74da768b40ec7b76df626
NatalieBlack <natalievictoriablack@gmail.com>
2016-04-19 16:37:59 -0400

    tone down hints in crm.rb and take out weird class method

--- crm.rb
+++ crm.rb
@@ -1,58 +1,50 @@
-# Add any require_relative statements here
-
 class CRM
 
-  def self.run(name)
-    # Fill this in
-  end
+  def initialize
 
-  def initialize(name)
-    # Fill this in
   end
 
   def main_menu
-    # Fill this in
+
   end
 
   def print_main_menu
-    # Fill this in
+
   end
 
-  def call_option(user_selected)
-    # Fill this in
+  def call_option
+
   end
 
   def add_new_contact
-    # Fill this in
+
   end
 
   def modify_existing_contact
-    # Fill this in
+
   end
 
   def delete_contact
-    # Fill this in
+
+  end
+
+  # This method should accept as an argument an array of contacts
+  # and display each contact in that array
+  def display_contacts
+
+    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
   end
 
   def display_all_contacts
-    # Fill this in
-    # HINT: Make use of the display_contacts method
+    
+    # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
   def search_by_attribute
-    # Fill this in
-    # HINT: Make use of the display_contacts method
-  end
-
-  # This method should accept as its argument an array of contacts
-  # and display each contact in the contacts array
-  def display_contacts(contacts)
-    # Fill this in
-    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods
+    
+    # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
   # Add other methods here, if you need them.
 
 end
-
-# Run the program here (See 'Using a class method`)
