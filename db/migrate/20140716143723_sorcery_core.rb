4767f5acdc4b4190ac5565f2a5a3c1a3c65c13bd
NatalieBlack <natalievictoriablack@gmail.com>
2016-11-24 14:25:06 -0500

    upgrade to rails 5

--- db/migrate/20140716143723_sorcery_core.rb
+++ db/migrate/20140716143723_sorcery_core.rb
@@ -1,6 +1,5 @@
 class SorceryCore < ActiveRecord::Migration
   def change
-    drop_table :users
 
     create_table :users do |t|
       t.string :first_name
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20140717150139) do
 
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "goal"
@@ -24,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140717150139) do
     t.string   "image"
   end
 
-  create_table "rewards", force: true do |t|
+  create_table "rewards", force: :cascade do |t|
     t.integer  "project_id"
     t.string   "description"
     t.integer  "amount"
@@ -32,27 +31,25 @@ ActiveRecord::Schema.define(version: 20140717150139) do
     t.datetime "updated_at"
   end
 
-  create_table "taggings", force: true do |t|
+  create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
-    t.integer  "taggable_id"
     t.string   "taggable_type"
-    t.integer  "tagger_id"
+    t.integer  "taggable_id"
     t.string   "tagger_type"
+    t.integer  "tagger_id"
     t.string   "context",       limit: 128
     t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email",                                        null: false
@@ -63,9 +60,8 @@ ActiveRecord::Schema.define(version: 20140717150139) do
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.boolean  "admin",                        default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
 
 end
