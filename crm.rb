6fac774ff08c8c9fd9ea2856a0c13f6c34552741
NatalieBlack <natalievictoriablack@gmail.com>
2017-01-18 16:43:12 -0500

    merge display_contacts and display_all_contacts

--- crm.rb
+++ crm.rb
@@ -28,23 +28,13 @@ class CRM
 
   end
 
-  # This method should accept as an argument an array of contacts
-  # and display each contact in that array
-  def display_contacts
-
-    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
-  end
-
   def display_all_contacts
     
-    # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
   def search_by_attribute
     
-    # HINT: Make use of the display_contacts method to keep your code DRY
   end
 
-  # Add other methods here, if you need them.
 
 end
