d0b71bc68847840060703f58da96276fa3b0a259
Sean Lerner <sean@smallcity.ca>
2017-05-26 15:57:49 -0400

    added comments to files that should not be modified

--- base/episode.rb
+++ base/episode.rb
@@ -1,3 +1,11 @@
+# IMPORTANT: DO NOT MODIFY THIS FILE.
+# To complete this assignment you only need to modify
+# the files inside the individual /episode-xx/ folders.
+# You may, however, take a look inside this file to see
+# how the entire program works. Ideally though, you'll
+# only need to work with the files inside the individual
+# /episode-xx/ folders to fix the replicator each episode.
+
 class Episode
 
   EPISODE_NUMBERS = Dir.glob("episode-*").map {|folder| folder[/\d+$/] }
