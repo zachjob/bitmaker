a331435c3dd7f18a641ab4a7bcc0cb4f1a488f52
Daniel Moniz <daniel.moniz@gmail.com>
2017-06-19 13:31:35 -0400

    Set up PledgesControllerTest for testing.

--- test/controllers/pledges_controller_test.rb
+++ test/controllers/pledges_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
-class PledgesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+class PledgesControllerTest < ActionController::TestCase
+  # test "create redirects to login when logged out" do
+  #   logout_user
+  #   project = FactoryGirl.create(:pledge)
+  #   post :create, params: { pledge: pledge.as_json }
+  #   assert_redirected_to login_url
   # end
 end
