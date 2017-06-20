b685c72b99d0d3318afdb28355fc2aa17b86ef02
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 19:05:18 -0400

    Create migration to remove reward_id and add project_id to pledges

--- db/schema.rb
+++ db/schema.rb
@@ -10,55 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130151807) do
+ActiveRecord::Schema.define(version: 20170620230230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pledges", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "reward_id"
-    t.float    "dollar_amount"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["reward_id"], name: "index_pledges_on_reward_id", using: :btree
-    t.index ["user_id"], name: "index_pledges_on_user_id", using: :btree
+  create_table "pledges", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.float "dollar_amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_pledges_on_project_id"
+    t.index ["user_id"], name: "index_pledges_on_user_id"
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "goal"
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "goal"
     t.datetime "start_date"
     t.datetime "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string "image"
   end
 
-  create_table "rewards", force: :cascade do |t|
-    t.integer  "project_id"
-    t.string   "description"
-    t.float    "dollar_amount"
+  create_table "rewards", id: :serial, force: :cascade do |t|
+    t.integer "project_id"
+    t.string "description"
+    t.float "dollar_amount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email",                                        null: false
-    t.string   "crypted_password",                             null: false
-    t.string   "salt",                                         null: false
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", null: false
+    t.string "crypted_password", null: false
+    t.string "salt", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_me_token"
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.boolean  "admin",                        default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
 
-  add_foreign_key "pledges", "rewards"
+  add_foreign_key "pledges", "projects"
   add_foreign_key "pledges", "users"
 end
