9e98e8617c996d5ded35ea542c9b57fdc5b8c1a4
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 12:13:27 -0400

    finish removing factories and fixtures from tests

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -7,13 +7,13 @@ class ProjectsControllerTest < ActionController::TestCase
   end
 
   test "show returns success" do
-    project = FactoryGirl.create(:project)
+    project = Project.create(title: "Test Project", description: "A wonderful project", goal: 10000, start_date: 1.day.from_now, end_date: 30.days.from_now)
     get :show, params: {id: project.id}
     assert_response :success
   end
 
   test "new returns success when logged in" do
-    user = FactoryGirl.create(:user)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     login_user(user = user)
     get :new
     assert_response :success
@@ -26,22 +26,22 @@ class ProjectsControllerTest < ActionController::TestCase
   end
 
   test "create redirects to login when logged out" do
-    project = FactoryGirl.create(:project)
+    project = Project.create(title: "Test Project", description: "A wonderful project", goal: 10000, start_date: 1.day.from_now, end_date: 30.days.from_now)
     logout_user
     post :create, params: { project: project.as_json }
     assert_redirected_to login_url
   end
 
   test "create renders new when logged in and invalid" do
-    user = FactoryGirl.create(:user)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     login_user(user = user)
     post :create, params: { project: {title: 'test'} } # missing required params
     assert_template :new
   end
 
   test "create redirects to projects when logged in and valid" do
-    user = FactoryGirl.create(:user)
-    project = FactoryGirl.create(:project)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
+    project = Project.create(title: "Test Project", description: "A wonderful project", goal: 10000, start_date: 1.day.from_now, end_date: 30.days.from_now)
     login_user(user = user)
     post :create, params: { project: project.as_json }
     assert_redirected_to projects_url
--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -7,19 +7,19 @@ class UserSessionsControllerTest < ActionController::TestCase
   end
 
   test "create returns a success on correct email and password" do
-    user = FactoryGirl.create(:user)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     post :create, params: {user: {email: user.email, password: user.password}}
     assert_response :success
   end
 
   test "create renders new on failure" do
-    user = FactoryGirl.create(:user)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     post :create, params: {user: {email: user.email, password: 'invalid'}}
     assert_template :new
   end
 
   test "destroy redirects to projects when logged in" do
-    user = FactoryGirl.create(:user)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     login_user(user = user)
     delete :destroy
     assert_redirected_to root_url
