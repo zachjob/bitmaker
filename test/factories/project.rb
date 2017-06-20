6f724949a8a78b6159886681767dfc14714b5cff
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 12:00:59 -0400

    remove project factory

--- test/factories/project.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :project do
-    title "test"
-    description "test"
-    goal 1
-    start_date DateTime.now
-    end_date DateTime.now
-  end
-end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  test "project is valid with default factory" do
-    project = FactoryGirl.build(:project)
-    assert project.valid?
-  end
 end
