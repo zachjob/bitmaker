cc3985bdbf224b15a9df31e6240bfca2c9c248b6
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 12:00:08 -0400

    remove user factory

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,28 +2,23 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "email must be unique" do
-    user = FactoryGirl.create(:user, :email_no_sequence)
-    user2 = FactoryGirl.build(:user, :email_no_sequence)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
+    user2 = User.create(email: "bettymaker@gmail.com", password: "00000000", password_confirmation: "00000000")
     refute user2.valid?
   end
 
   test "user must include password_confirmation on create" do
-    user = FactoryGirl.build(:user, :no_confirm)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678")
     refute user.valid?
   end
 
   test "password must match confirmation" do
-    user = FactoryGirl.build(:user, password: 'password1', password_confirmation: 'password2')
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "87654321")
     refute user.valid?
   end
 
   test "password must be at least 8 characters long" do
-    user = FactoryGirl.build(:user, password: '1234', password_confirmation: '1234')
+    user = User.create(email: "bettymaker@gmail.com", password: "1234", password_confirmation: "1234")
     refute user.valid?
-  end
-
-  test "user is valid with default factory" do
-    user = FactoryGirl.build(:user)
-    assert user.valid?
   end
 end
