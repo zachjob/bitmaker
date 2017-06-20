6b7c5fc3b7e0f8517bbe799bc67617f6c8d9dae6
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 18:45:19 -0400

    Remove accepts_nested_attributes_for from Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,5 @@ class Project < ActiveRecord::Base
   has_many :pledges, through: :rewards
   has_many :backers, through: :rewards
 
-  accepts_nested_attributes_for :rewards
-
   validates :title, :description, :goal, :start_date, :end_date, presence: true
 end
