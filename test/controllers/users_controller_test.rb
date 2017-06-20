7f691d48df5450a878bfc19de2e22dd95e86cadb
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 12:03:27 -0400

    use new instead of create where needed

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,21 +7,21 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "create adds a new user" do
-    user = FactoryGirl.build(:user)
+    user = User.new(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     assert_difference('User.count') do
       post :create, params: {user: {email: user.email, password: user.password, password_confirmation: user.password_confirmation}}
     end
   end
 
   test "create redirects to projects page on success" do
-    user = FactoryGirl.build(:user)
+    user = User.new(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     post :create, params: {user: {email: user.email, password: user.password, password_confirmation: user.password_confirmation}}
     assert_redirected_to projects_url
   end
 
   test "create renders new on failure" do
-    user = FactoryGirl.create(:user, :email_no_sequence)
-    user2 = FactoryGirl.build(:user, :email_no_sequence)
+    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
+    user2 = User.new(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
     post :create, params: {user: {email: user2.email, password: user2.password, password_confirmation: user2.password_confirmation}}
     assert_template :new
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,22 +3,22 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "email must be unique" do
     user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
-    user2 = User.create(email: "bettymaker@gmail.com", password: "00000000", password_confirmation: "00000000")
+    user2 = User.new(email: "bettymaker@gmail.com", password: "00000000", password_confirmation: "00000000")
     refute user2.valid?
   end
 
   test "user must include password_confirmation on create" do
-    user = User.create(email: "bettymaker@gmail.com", password: "12345678")
+    user = User.new(email: "bettymaker@gmail.com", password: "12345678")
     refute user.valid?
   end
 
   test "password must match confirmation" do
-    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "87654321")
+    user = User.new(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "87654321")
     refute user.valid?
   end
 
   test "password must be at least 8 characters long" do
-    user = User.create(email: "bettymaker@gmail.com", password: "1234", password_confirmation: "1234")
+    user = User.new(email: "bettymaker@gmail.com", password: "1234", password_confirmation: "1234")
     refute user.valid?
   end
 end
