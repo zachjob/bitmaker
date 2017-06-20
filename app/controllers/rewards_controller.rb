d8982ee391289bac820b62fdd3760edef9995266
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 19:15:11 -0400

    Create view for creating a new Reward

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,21 @@
 class RewardsController < ApplicationController
+  before_action :load_project
+
+  def new
+    @reward = Reward.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def load_project
+    @project = Project.find(params[:project_id])
+  end
 end
