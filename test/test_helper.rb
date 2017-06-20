95528b835fe4031638c97ed036546d78428f12e2
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 19:52:04 -0400

    Remove factory_girl_rails gem from project

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
-end
-
 # Include sorcery test helpers in controller tests
 class ActionController::TestCase
   include Sorcery::TestHelpers::Rails::Integration
