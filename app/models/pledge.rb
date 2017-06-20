0ec9ecab26adfc25fab1e657838324da202140cc
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 19:35:19 -0400

    Remove enough_pledged validation from Pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,12 +1,4 @@
 class Pledge < ApplicationRecord
   belongs_to :user
   belongs_to :project
-
-  validate :enough_pledged
-
-  def enough_pledged
-    unless self.dollar_amount >= reward.dollar_amount
-      self.errors.add(:dollar_amount, "pledged must be at least $#{reward.dollar_amount}")
-    end
-  end
 end
