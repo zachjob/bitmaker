75880f220ca73c83a7eb122fc2de53626956f9ee
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-20 13:41:56 -0400

    initial commit

---
+++ test_robot.rb
@@ -0,0 +1,79 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './robot.rb'
+
+class TestRobot < MiniTest::Test
+
+  def test_station_needs_repairs_foreign_and_vintage_model_returns_1
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_station_needs_repairs_foreign_model_returns_1
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_station_needs_repairs_vintage_model_returns_2
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_station_needs_repairs_returns_3
+    # arrange
+    
+    # act
+
+    # assert
+  end
+  
+  def test_station_good_condition_returns_4
+    # arrange
+    
+    # act
+
+    # assert
+  end
+  
+  def test_prioritize_tasks_empty_todo_list
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_prioritize_tasks_not_empty_returns_max_value
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_workday_on_day_off
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+  def test_workday_not_day_off
+    # arrange
+    
+    # act
+
+    # assert
+  end
+
+end
