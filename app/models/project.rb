fef5e9d1a8a81c45fc8aded339aa061019523690
Mina Mikhail <mina@fightingtheboss.com>
2017-06-20 16:49:23 -0400

    Use validates method over specific validates_presence_of helper

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,8 @@ class Project < ActiveRecord::Base
   has_many :rewards
   has_many :pledges, through: :rewards
   has_many :backers, through: :rewards
-  accepts_nested_attributes_for :rewards
-  validates_presence_of :title, :description, :goal, :start_date, :end_date
 
+  accepts_nested_attributes_for :rewards
+
+  validates :title, :description, :goal, :start_date, :end_date, presence: true
 end
