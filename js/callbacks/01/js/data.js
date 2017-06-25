cd0ed99c4d9a986235cf1e3b48fa55b64dd8cf58
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-25 19:20:48 -0400

    Updated sample data for 'js/callbacks/01' exercise.

--- js/callbacks/01/js/data.js
+++ js/callbacks/01/js/data.js
@@ -1,1 +1,108 @@
-var transactions = [];
+var transactions = [
+	{
+		type: 'sale',
+		paymentMethod: 'cash',
+		customer: 'Isaac Asimov',
+		items: [
+			{ name: 'Byte', price: 1.00 },
+			{ name: 'Bit', price: 0.125 }
+		]
+	},
+	{
+		type: 'sale',
+		paymentMethod: 'credit',
+		customer: 'CJ Cherryh',
+		items: [
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 }
+		]
+	},
+	{
+		type: 'purchase',
+		paymentMethod: 'credit',
+		vendor: 'Dick\'s Doodads',
+		items: [
+			{ name: 'XL Doodad', price: 3.00 },
+			{ name: 'XS Doodad', price: 0.50 }
+		]
+	},
+	{
+		type: 'purchase',
+		paymentMethod: 'cash',
+		customer: 'Gibson Gadgets',
+		items: [
+			{ name: 'Basic Gadget', price: 2.00 },
+			{ name: 'Advanced Gadget', price: 3.50  }
+		]
+	},
+	{
+		type: 'sale',
+		paymentMethod: 'credit',
+		customer: 'Frederik Pohl',
+		items: [
+			{ name: 'Byte', price: 1.00 },
+			{ name: 'Byte', price: 1.00 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 }
+		]
+	},
+	{
+		type: 'sale',
+		paymentMethod: 'cash',
+		customer: 'Clarke Computing',
+		items: [
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 },
+			{ name: 'Floppy Disk', price: 0.10 }
+		]
+	},
+	{
+		type: 'sale',
+		paymentMethod: 'credit',
+		customer: 'Neal Stephenson',
+		items: [
+			{ name: 'kilobyte', price: 1024.00 }
+		]
+	},
+	{
+		type: 'purchase',
+		paymentMethod: 'credit',
+		customer: 'Gibson Gadgets',
+		items: [
+			{ name: 'Advanced Gadget', price: 3.50  },
+			{ name: 'Advanced Gadget', price: 3.50  },
+			{ name: 'Advanced Gadget', price: 3.50  },
+			{ name: 'Advanced Gadget', price: 3.50  }
+		]
+	},
+	{
+		type: 'purchase',
+		paymentMethod: 'credit',
+		vendor: 'Dick\'s Doodads',
+		items: [
+			{ name: 'XL Doodad', price: 3.00 },
+			{ name: 'XL Doodad', price: 3.00 },
+			{ name: 'XL Doodad', price: 3.00 }
+		]
+	},
+	{
+		type: 'sale',
+		paymentMethod: 'cash',
+		customer: 'Isaac Asimov',
+		items: [
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+			{ name: 'Bit', price: 0.125 },
+		]
+	}
+];
