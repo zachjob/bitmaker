3b953b223360e7bf61d203485bf5c4b7765ea756
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-25 20:30:28 -0400

    Updated main/exercise file for 'js/callbacks/01': filled out questions/hints; updated instructions.

--- js/callbacks/01/js/main.js
+++ js/callbacks/01/js/main.js
@@ -2,24 +2,35 @@
 // INSTRUCTIONS
 // --------------------------------------------------
 /*
-- Before You Start:
-	- Open the `index.html` file in a browser (preferably Google Chrome).
-	- Open the JavaScript console.
-	- Ensure that information is being logged to the console.
-- To Start:
- 	- Read through the questions included below.
-	- Check out the example question and its solution.
-	- Answer and test each question before moving on to the next one.
-- To Test:
-	- Open the `index.html` file and JavaScript console in browser.
-	- Make sure that your solutions are being logged out.
-	- Make sure that there are no errors.
-- Hints:
-	- You will need to use the following Array methods:
-		- .filter()
-		- .map()
-		- .reduce()
-		- .sort()
+BEFORE YOU START
+------------------------------
+- Open the `index.html` file in a browser (preferably Google Chrome).
+- Open the JavaScript console.
+- Ensure that information is being logged to the console.
+
+TO START
+------------------------------
+- Read through the questions included below.
+- Check out the example question and its solution.
+- Check out the structure of the `transactions` objects in the 'data.js' file.
+- Answer and test each question before moving on to the next one.
+
+TO TEST
+------------------------------
+- Open the `index.html` file and JavaScript console in browser.
+- Make sure that your solutions are being logged out.
+- Make sure that there are no errors.
+
+HINTS
+------------------------------
+- Each solution requires you to operate on the contents of the `transactions` array.
+- At the very least, you will need to use the following Array methods:
+	- .filter()
+	- .map()
+	- .reduce()
+	- .sort()
+- Documentation for these methods (and the other 'Array Extras') can be found here:
+	- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Iteration_methods
 */
 
 
@@ -39,6 +50,9 @@ console.log( 'The total number of transactions is:', totalTransactions );
 // --------------------------------------------------
 /*
 	Calculate the total number of 'sales'.
+
+	HINT(S):
+	- Not all transactions are 'sales'.
 */
 var numSales;
 
@@ -63,7 +77,7 @@ console.log( 'The total number of purchases is:', numPurchases );
 	Calculate the total number of 'cash' 'sales'.
 
 	HINT(S):
-	Don't forget that 'purchases' can also be made in 'cash'!
+	- Don't forget that 'purchases' can also be made in 'cash'!
 */
 var numCashSales;
 
@@ -77,7 +91,7 @@ console.log( 'The total number of cash sales is:', numCashSales );
 	Calculate the total number of 'credit' 'purchases'.
 
 	HINT(S):
-	Make sure to exclude any 'sales' made by 'credit'!
+	- Make sure to exclude any 'sales' made by 'credit'!
 */
 var numCreditPurchases;
 
@@ -88,13 +102,13 @@ console.log( 'The total number of credit purchases is:', numCreditPurchases );
 // QUESTION 05
 // --------------------------------------------------
 /*
-	Create an array that includes all of 'unique vendors' which appear in the transactions data set.
+	Create an array that includes all of vendors which appear in the transactions data set.
 	eg. `[ 'vendor one', 'vendor two', ... ]
 
 	HINT(S):
-	Not all transactions have a 'vendor'!
-	The assembled array should be made up of strings, not full `transaction` objects.
-	The name of each vendor should appear only once (eg. no duplicates).
+	- Not all transactions have a 'vendor'!
+	- The assembled array should be made up of strings, not full `transaction` objects.
+	- This array is allowed to contain duplicate values.
 */
 var uniqueVendors;
 
@@ -105,7 +119,13 @@ console.log( 'The unique vendors are:', uniqueVendors );
 // QUESTION 06
 // --------------------------------------------------
 /*
-	UNIQUE CUSTOMERS ARRAY /// TEMP
+	Create an array that includes all of *unique* customers which appear in the transactions data set.
+	eg. `[ 'customer one', 'customer two', ... ]
+
+	HINT(S):
+	- Not all transactions have a 'customer'!
+	- The assembled array should be made up of strings, not full `transaction` objects.
+	- Make sure that the resulting array *does not* include any duplicates.
 */
 var uniqueCustomers;
 
@@ -116,7 +136,14 @@ console.log( 'The unique customers are:', uniqueCustomers );
 // QUESTION 07
 // --------------------------------------------------
 /*
-	'BIG SPENDERS' ARRAY /// TEMP
+	Create an array of information about the 'sale' transactions which include 5 or more items.
+
+	The array should resemble the following:
+	[ { name: 'Customer Name', numItems: 5 }, ... ]
+
+	HINT(S):
+	- There may be more than 1 'sale' that includes 5 or more items.
+	- Individual transactions do not have either `name` or `numItems` properties, we'll have to add them to the output.
 */
 var bigSpenders;
 
@@ -127,7 +154,10 @@ console.log( 'The "big spenders" are:', bigSpenders );
 // QUESTION 08
 // --------------------------------------------------
 /*
-	TOTAL AMOUNT SOLD (SUM) /// TEMP
+	Calculate the sum of the *first* 'sale' transaction.
+
+	HINT(S):
+	- Transactions don't have 'prices', but their 'items' do!
 */
 var sumSales;
 
@@ -138,8 +168,13 @@ console.log( 'The sum of all sales is:', sumSales );
 // QUESTION 09
 // --------------------------------------------------
 /*
-	TOTAL CAD PURCHASES
+	Calculate the sum of *all* 'purchase' transactions.
+
+	HINT(S):
+	- Your solution to 'QUESTION 08' is a good starting point!
+	- Make sure to include 'price' information from *all* purchases.
 */
+
 var sumPurchases;
 
 console.log( 'The sum of all purhcases is:', sumPurchases );
@@ -149,7 +184,14 @@ console.log( 'The sum of all purhcases is:', sumPurchases );
 // QUESTION 10
 // --------------------------------------------------
 /*
-	NET PROFIT /// TEMP
+	Calculate the company's net profit.
+
+	This number will be positive if the sum of the sales is greater than the amount spent on purchases.
+
+	Otherwise, this number will be negative.
+
+	HINT(S):
+	- Unlike 'QUESTION 08' and 'QUESTION 09', here we're interested in both 'sale' and 'purchase' transactions.
 */
 var netProfit;
 
@@ -160,7 +202,10 @@ console.log( 'The net profit is:', netProfit );
 // QUESTION 11
 // --------------------------------------------------
 /*
-	MOST ITEMS SOLD AS PART OF A SINGLE TRANSACTION /// TEMP
+	Calculate the most items sold as part of single transaction.
+
+	HINTS:
+	- The result of this calculation should be a number (not an array, object, or other data type).
 */
 var mostItems;
 
@@ -171,8 +216,8 @@ console.log( 'The most items sold in a single transaction is:', mostItems );
 // QUESTION 12
 // --------------------------------------------------
 /*
-	FEWEST ITEMS SOLD AS PART OF A SINGLE TRANSACTION /// TEMP
+	Calculate the sum of the 'purchase' with the fewest items.
 */
-var fewestItems;
+var sumOfSmallestPurchase;
 
-console.log( 'The fewest items sold in a single transaction is:', fewestItems );
+console.log( 'The sum of the smallest purchase is:', sumOfSmallestPurchase );
