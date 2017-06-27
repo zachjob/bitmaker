50b4538584020c631801c5081396fc2c74b37102
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-26 21:32:28 -0400

    Added additional 'getting started' notes in 'main.js'.

--- main.js
+++ main.js
@@ -131,6 +131,25 @@ console.log( 'The total number of transactions is:', totalTransactions );
 */
 var numSales;
 
+/*
+	Hey, welcome to the first question!
+
+	Here's a breakdown of the question, and some pointers on how to get started!
+		- A variable has been declared a few lines above (`numSales`).
+		- Just below, the contents of the `numSales` variable are logged to the console.
+		- Your job is to assign the variable to the correct value (in this case: the total number of sales) *BEFORE* it is logged out.
+		- You can do this by:
+			- Adding an `=` sign (we are *assigning* something after all)
+			- Starting with the `transactions` variable (see the example question);
+			- Adding one or more methods to transform/extract the value we're looking for.
+		- If your solution is correct, `numSales` should be equal to 5.
+
+	You can solve the remaining questions in the same way!
+
+	P.S.
+	The breakdown above takes up a lot of space, feel free to move it to the top or bottom of the file!
+*/
+
 console.log( 'The total number of sales is:', numSales );
 
 
