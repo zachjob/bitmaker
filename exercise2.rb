8f4314d8cf5ed5fe000f8c53fe28f564c6561078
Zach Job <zachery.job@gmail.com>
2017-09-18 12:51:59 -0400

    Convert an integer to a string using .to_s

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,5 @@
 # How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
-puts 55 * 0.2
+# puts 55 * 0.2
+
+# Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+puts "Hello" + 55.to_s
