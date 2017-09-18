c0a0a32fc015c26a1c224dfc75020b6fbd685743
Zach Job <zachery.job@gmail.com>
2017-09-18 12:58:54 -0400

    Complete exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -5,4 +5,7 @@
 # puts "Hello" + 55.to_s
 
 # Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
-puts "45628 multiplied by 7839 is #{45628 * 7839}"
+# puts "45628 multiplied by 7839 is #{45628 * 7839}"
+
+# What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+puts "(10 is less than 20) is true and (30 is less than 20) is false OR !(10 equals 11) is true; This expression is true since one part of it is true and it is foremost an or-statement"
