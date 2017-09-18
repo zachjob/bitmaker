6b4617b4dfbce04a9723d675f2a06f13e85dc8c5
Zach Job <zachery.job@gmail.com>
2017-09-18 12:54:00 -0400

    Output an equation in sentence form using string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -2,4 +2,7 @@
 # puts 55 * 0.2
 
 # Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
-puts "Hello" + 55.to_s
+# puts "Hello" + 55.to_s
+
+# Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
