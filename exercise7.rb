6591983df2ca4106997a07ff5a4660211c2ebedf
Zach Job <zachery.job@gmail.com>
2017-09-19 14:43:33 -0400

    Update method to allow user to select a symbol for multiple inputs

--- exercise7.rb
+++ exercise7.rb
@@ -1,8 +1,15 @@
 puts "Enter a string"
 user_string = gets.to_s
+user_string = user_string.delete("\n")
 
 def wrap_text (my_string)
-  user_string = "<<< #{my_string} >>>"
+  puts "Enter a Symbol"
+  user_symbol = gets.to_s
+  user_symbol = user_symbol.delete("\n")
+
+  return user_string = "#{user_symbol}#{user_symbol}#{user_symbol}#{my_string}#{user_symbol}#{user_symbol}#{user_symbol}"
 end
 
+puts wrap_text (user_string)
+puts wrap_text (user_string)
 puts wrap_text (user_string)
