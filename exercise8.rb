d1bf3d0cc5d405eb0715d7f90bfbbfe7b98f7bc1
Zach Job <zachery.job@gmail.com>
2017-09-19 15:24:39 -0400

    Replace 3 methods with one, complete exercise 8

--- exercise8.rb
+++ exercise8.rb
@@ -1,18 +1,10 @@
-def person_distance (user_distance)
+def person_speed (user_speed)
   puts "How far did person run (in metres)?"
   user_distance = gets.to_f
 
-  return user_distance
-end
-
-def person_mins (user_mins)
-  puts "How long (in minutes) did person run?"
+  puts "How long (in minutes) did it take for person to run #{user_distance}?"
   user_mins = gets.to_f
 
-  return user_mins
-end
-
-def person_speed (user_mins, user_distance)
   user_secs = user_mins * 60
   speed = user_distance/user_secs
 
@@ -20,18 +12,9 @@ def person_speed (user_mins, user_distance)
 end
 
 
-distance1 = person_distance (distance1)
-mins1 = person_mins (mins1)
-
-distance2 = person_distance (distance2)
-mins2 = person_mins (mins2)
-
-distance3 = person_distance (distance3)
-mins3 = person_mins (mins3)
-
-speed1 = person_speed mins1, distance1
-speed2 = person_speed mins2, distance2
-speed3 = person_speed mins3, distance3
+speed1 = person_speed speed1
+speed2 = person_speed speed2
+speed3 = person_speed speed3
 
 if speed3 > speed2 && speed3 > speed1
   puts "Person 3 was the fastest at #{speed3} m/s"
