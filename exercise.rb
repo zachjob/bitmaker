620dbd050e0fbf5adbfa6614c3056971fd87ab0c
Zach Job <zachery.job@gmail.com>
2017-09-20 16:32:21 -0400

    Create arrays for exercise 0

---
+++ exercise.rb
@@ -0,0 +1,43 @@
+# Exercise 0
+# your favourite colours
+fav_colours = [
+  "red",
+  "purple",
+  "indigo",
+  "cyan",
+  "cerulean"
+]
+
+
+# the age of you and your siblings/cousins/friends
+age = [
+  "19",
+  "20",
+  "23",
+  "27"
+]
+
+# flip a coin 5 times and record whether or not the result was 'heads'
+coin_flip = [
+  "heads",
+  "heads",
+  "tails",
+  "tails",
+  "tails"
+]
+
+# your three favourite performing artists
+artists = [
+  "Imagine Dragons",
+  "Above & Beyond",
+  "Andrew Bayer"
+]
+
+# your favourite colours again (this time as symbols instead of strings)
+colour_symbols = [
+  :red,
+  :purple,
+  :indigo,
+  :cyan,
+  :cerulean
+]
