79a9342c89e574d2210cc2e7cb2a749bd937571a
Zach Job <zachery.job@gmail.com>
2017-09-21 15:58:40 -0400

    Create a method to display the number of students in each cohort

---
+++ exercise9.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def student_population (class_size)
+  class_size.each do |cohort, num_students|
+    puts "#{cohort}: #{num_students} students"
+  end
+end
+
+student_population students
