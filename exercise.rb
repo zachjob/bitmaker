9ce4f4c029d0efb59eb7edde3502b599515efba6
Zach Job <zachery.job@gmail.com>
2017-09-29 13:26:25 -0400

    Create Task class and reader methods

---
+++ exercise.rb
@@ -0,0 +1,16 @@
+class Task
+
+  def initialize (description, due_date)
+    @description = description
+    @due_date = due_date
+  end
+
+  def description
+    @description
+  end
+
+  def due_date
+    @due_date
+  end
+
+end
