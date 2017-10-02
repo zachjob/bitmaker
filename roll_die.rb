eb2b6cff920df54d51bce9be8b1035114b80ed30
Zach Job <zachery.job@gmail.com>
2017-10-02 16:10:20 -0400

    Add value checker to make sure 0 isn't rolled

--- roll_die.rb
+++ roll_die.rb
@@ -1,9 +1,12 @@
-rolls = []
-
 def roll_die
   roll = 0
   roll = Random.rand(7)
-  "The result of your roll is #{roll}."
+  if roll > 0
+    "The result of your roll is #{roll}."
+  else
+    roll = Random.rand(7)
+    "The result of your roll is #{roll}."
+  end
 end
 
 puts roll_die
