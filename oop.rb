2d811fbb0eb5064d2fb861b60f05f18bf4131bfd
Zach Job <zachery.job@gmail.com>
2017-10-04 14:13:22 -0400

    Complete exercise

--- oop.rb
+++ oop.rb
@@ -1,7 +1,12 @@
 class Location
 
   def initialize
+    puts "Type the Destination:"
     @name = gets
+  end
+
+  def show_name
+    "#{@name}"
   end
 
 end
@@ -12,8 +17,19 @@ class Trip
     @destinations = []
   end
 
-  def trip_log (destinations)
-
+  def add_location
+    @destinations << Location.new
   end
 
+  def trip_log
+    i = 0
+    @destinations.length.times
+      puts "I went from #{@destinations[i].show_name} to #{@destinations[i+1].show_name}"
+    end
+
 end
+
+canada = Trip.new
+canada.add_location
+canada.add_location
+canada.trip_log
