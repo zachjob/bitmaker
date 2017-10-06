fbb333a76b51a6ec61f1b5808154c4f7ea039bce
Zach Job <zachery.job@gmail.com>
2017-10-06 14:27:31 -0400

    Create and run eats_at method

--- meow.rb
+++ meow.rb
@@ -6,7 +6,20 @@ class Cat
     @meal_time = feed.to_i
   end
 
+  def eats_at
+
+    if @meal_time >= 12
+      "#{@meal_time} PM"
+    else
+      "#{@meal_time} AM"
+    end
+
+  end
+
 end
 
 nex = Cat.new("Nex", "Salmon", 12)
 sam = Cat.new("Sam", "Tuna", 11)
+
+puts nex.eats_at
+puts sam.eats_at
