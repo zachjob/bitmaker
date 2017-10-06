ec59ac856e2f4557da3015233edaff39c051e2dc
Zach Job <zachery.job@gmail.com>
2017-10-06 15:06:17 -0400

    Creater reader methods for each variable and complete do_battle method

--- player.rb
+++ player.rb
@@ -22,12 +22,31 @@ def Player
     if @gold_coins % 10 = 0
       self.level_up
     end
-
-
   end
 
-  def do_battle
+  def do_battle (damage)
+    @health_points -= damage
 
+    if @health_points <= 0
+      @lives -= 1
+      if @lives <= 0
+        self.restart
+      end
+    end
+  end
+
+  def health
+    @health_points
+  end
+
+  def gold
+    @gold_coins
+  end
+
+  def lives
+    @lives
   end
 
 end
+
+zach = Player.new
