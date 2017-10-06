c2ee16104817faedcb90dc7d21178812ace781ef
Zach Job <zachery.job@gmail.com>
2017-10-06 13:38:18 -0400

    Remove excess methods and calls

--- bank.rb
+++ bank.rb
@@ -1,24 +1,5 @@
 class BankAccount
   @@interest_rate = 1.0115
-  @@accounts = []
-
-  def self.create
-    @@accounts << BankAccount.new
-    BankAccount.new
-  end
-
-  def self.total_funds
-    account = 0
-    @@accounts.each do |amount|
-      account += amount.update
-    end
-  end
-
-  def self.interest_time
-    @@accounts.each do |amount|
-      @balance = amount.update*@@interest_rate
-    end
-  end
 
   def initialize
     @balance = 0
@@ -26,10 +7,6 @@ class BankAccount
 
   def balance
     @balance.to_s
-  end
-
-  def update
-    @balance
   end
 
   def deposit (amount)
@@ -41,22 +18,3 @@ class BankAccount
   end
 
 end
-
-my_account = BankAccount.create
-
-my_account = BankAccount.create
-your_account = BankAccount.create
-puts my_account.balance # 0
-puts BankAccount.total_funds # 0
-my_account.deposit(200)
-your_account.deposit(1000)
-puts my_account.balance # 200
-puts your_account.balance # 1000
-puts BankAccount.total_funds # 1200
-BankAccount.interest_time
-puts my_account.balance # 202.0
-puts your_account.balance # 1010.0
-puts BankAccount.total_funds # 1212.0
-my_account.withdraw(50)
-puts my_account.balance # 152.0
-puts BankAccount.total_funds # 1162.0
