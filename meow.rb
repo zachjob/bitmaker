274b778eb1bfc02acbf8e6b31b0069f146f9f964
Zach Job <zachery.job@gmail.com>
2017-10-06 14:05:56 -0400

    Create Nex & Sam instances.

--- meow.rb
+++ meow.rb
@@ -1,9 +1,12 @@
 class Cat
 
-  def initialize
-    @name = gets
-    @preferred_food = gets
-    @meal_time = gets.to_i
+  def initialize (name, food, feed)
+    @name = name
+    @preferred_food = food
+    @meal_time = feed.to_i
   end
 
 end
+
+nex = Cat.new("Nex", "Salmon", 12)
+sam = Cat.new("Sam", "Tuna", 11)
