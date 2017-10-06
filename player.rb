df674fd89e9adfa06885c94aab26808b19912079
Zach Job <zachery.job@gmail.com>
2017-10-06 15:01:19 -0400

    Complete initialize, restart, level_up, and collect_treasure methods

--- player.rb
+++ player.rb
@@ -1,18 +1,28 @@
 def Player
 
   def initialize
-
+    @gold_coins = 0
+    @lives = 5
+    @health_points = 10
   end
 
   def restart
-
+    @gold_coins = 0
+    @lives = 5
+    @health_points = 10
   end
 
   def level_up
-
+    @lives += 1
   end
 
   def collect_treasure
+    @gold_coins +=1
+
+    if @gold_coins % 10 = 0
+      self.level_up
+    end
+
 
   end
 
