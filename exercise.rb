faa1ec149e7c541097bd6edd25be2e7776e7bcee
Zach Job <zachery.job@gmail.com>
2017-10-10 14:23:20 -0400

    Create translated hash to store arrays in

---
+++ exercise.rb
@@ -0,0 +1,16 @@
+# Beginning arrays
+digits = ['1','2','3','4','5','6','7','8','9']
+en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+
+# Translated into a hash
+
+translated = {}
+
+digits.each do |num|
+  en.each do |eng|
+    translated[num] = eng
+  end
+end
+
+puts translated
