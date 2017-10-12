59b3f9b570cb5e4bce016d4db68896038b8a3e0e
Zach Job <zachery.job@gmail.com>
2017-10-12 14:33:09 -0400

    Create class variables, class methods, and instance methods

---
+++ books.rb
@@ -0,0 +1,48 @@
+class Book
+  @@on_shelf = []
+  @@on_loan = []
+
+  # Class Methods
+  def self.create
+    #code
+  end
+
+  def self.current_due_date
+    #code
+  end
+
+  def self.overdue_books
+    #code
+  end
+
+  def self.browse
+    #code
+  end
+
+  def self.available
+    #code
+  end
+
+  def self.borrowed
+    #code
+  end
+
+
+  # Instance Methods
+  def initialize
+    #code
+  end
+
+  def borrow
+    #code
+  end
+
+  def return_to_library
+    #code
+  end
+
+  def lent_out?
+    #code
+  end
+
+end
