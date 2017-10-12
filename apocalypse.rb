dd0540548e5834dd350a55960a0a222fa70c2914
Zach Job <zachery.job@gmail.com>
2017-10-12 16:14:35 -0400

    Create class and instance variables

--- apocalypse.rb
+++ apocalypse.rb
@@ -6,5 +6,42 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
+  # Instance Methods
+  def initialize
+    #code
+  end
+
+  def encounter
+    #code
+  end
+
+  def outrun_zombie?
+    #code
+  end
+
+  def survive_attack?
+    #code
+  end
+
+  # Class Methods
+  def self.all
+    #code
+  end
+
+  def self.new_day
+    #code
+  end
+
+  def self.some_die_off
+    #code
+  end
+
+  def self.spawn
+    #code
+  end
+
+  def self.increase_plague_level
+    #code
+  end
 
 end
