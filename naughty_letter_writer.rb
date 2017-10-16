0bbbbc9ace0c1aa96e5213c7fa4fb46019261cc6
Zach Job <zachery.job@gmail.com>
2017-10-16 14:59:34 -0400

    Create nice letters. Create naughty letter writer and template

---
+++ naughty_letter_writer.rb
@@ -0,0 +1,24 @@
+require 'erb'
+
+kids_data   = File.read('data/kids-data.txt')
+naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
+
+kids_data.each_line do |kid|
+
+  kid_data_array = kid.split
+
+  name     = kid_data_array[0]
+  behavior = kid_data_array[1]
+  toys     = kid_data_array[2..7]
+
+  next unless behavior == 'naughty'
+
+  filename    = 'letters/naughty/' + name + '.txt'
+  letter_text = ERB.new(naughty_letter, nil, '-').result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+
+end
+
+puts 'Done!'
