f7a3c9ca5c8b0f258cedcb64208b3d74c3b758e0
Zach Job <zachery.job@gmail.com>
2017-10-17 14:51:44 -0400

    Create cart methods for managing the cart

---
+++ cart.rb
@@ -0,0 +1,27 @@
+require 'products.rb'
+
+class Cart
+
+  @@contents = []
+
+  def initialize
+
+  end
+
+  def add_to_cart
+
+  end
+
+  def remove_from_cart
+
+  end
+
+  def before_taxes
+
+  end
+
+  def total
+
+  end
+
+end
---
+++ products.rb
@@ -0,0 +1,9 @@
+class Product
+
+  def initialize(name, price, taxes)
+    @name = name
+    @price = price
+    @taxes = taxes
+  end
+
+end
