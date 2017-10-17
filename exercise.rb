4ec0386208fba7c0dda4c17cab4586010a6da2c6
Zach Job <zachery.job@gmail.com>
2017-10-17 13:29:58 -0400

    Complete exercises 1-3

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,22 @@
-emotions = {
-  happy: 1,
-  sad: 2,
-  afraid: 1,
-}
+class Person
+
+  def initialize(name, emotions)
+    @name = name
+    @emotions = emotions
+  end
+
+  def levels
+    @emotions.each do |emotion, level|
+      if level >= 3
+        puts "I feel a high level of #{emotion}."
+      elsif level == 2
+        puts "I feel a moderate level of #{emotion}."
+      else
+        puts "I feel a low level of #{emotion}."
+      end
+    end
+  end
+end
+
+zach = Person.new("Zach", { happiness: 3, stress: 2, fear: 1 })
+zach.levels
