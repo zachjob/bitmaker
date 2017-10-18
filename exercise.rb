7718f2b5a75d05e9f4b99445cee4f2be79069182
Zach Job <zachery.job@gmail.com>
2017-10-18 10:14:48 -0400

    Use string indexing for proper accountability of all use-cases

--- exercise.rb
+++ exercise.rb
@@ -1,13 +1,11 @@
 def ordinal(num)
   # 1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th, 10th
   # 20th, 30th, 40th, 50th, 60th, 70th, 80th, 90th, 100th
-  if num % 10 == 0
-    puts "#{num}th."
-  elsif num - 1 == 0
+  if num.to_s[-1] == "1"
     puts "#{num}st."
-  elsif num - 2 == 0
+  elsif num.to_s[-1] == "2"
     puts "#{num}nd."
-  elsif num - 3 == 0
+  elsif num.to_s[-1] == "3"
     puts "#{num}rd."
   else
     puts "#{num}th."
@@ -16,5 +14,7 @@ end
 
 user_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 user_num.each do |num|
-  ordinal num.to_i
+  ordinal num
 end
+
+ordinal 1001
