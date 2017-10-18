bf8af7b0cd3d23b3e81236cd428d1492c77119be
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 11:33:42 -0400

    migrate and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  { name: 'Star Wars',
+  price_in_cents: 299,
+  description: "In the stars" },
+  { name: 'Lord of the Rings',
+  price_in_cents: 299,
+  description: "Some guys go to a volcano to destroy some other guy's ring." },
+  {name: 'Fast and Furious',
+  price_in_cents: 299,
+  description: "Fast cars. Faster characters."},
+  {name: "There will be blood",
+  price_in_cents: 399,
+  description: "Oil? Milkshakes?"}])
