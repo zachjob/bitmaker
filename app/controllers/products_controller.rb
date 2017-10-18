913f174afad6b253131e2bacc2f0199b8e7ef7fb
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 12:16:46 -0400

    Create form partial and set controller methods for new and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,21 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+def product_params
+  params.require(:product).permit(:name, :description, :price_in_cents)
+end
 
 end
