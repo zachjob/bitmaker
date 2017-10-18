6c2e5664f990cd087318a3f75eee65cb704823b7
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 12:51:55 -0400

    Create controller method delete a record and link on the edit and show pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,10 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
@@ -34,7 +38,9 @@ class ProductsController < ApplicationController
     end
   end
 
-  def product_params
-    params.require(:product).permit(:name, :description, :price_in_cents)
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 end
