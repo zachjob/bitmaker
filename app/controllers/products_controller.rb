ec2e6a1f2444624b31e447ad82476d44ea3aa4c3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-18 12:35:28 -0400

    Create links to show page, and set @product variables for show and edit pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def new
