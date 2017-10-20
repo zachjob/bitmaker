12ae803528b84583f14e57d33c1146281f8285c8
Zach Job <zachery.job@gmail.com>
2017-10-20 09:51:53 -0400

    Use each_with_index to assign row numbers and display each row and its contents

--- exercise.rb
+++ exercise.rb
@@ -4,3 +4,12 @@ seats = [
   ["Gismo", "Shadow", nil, nil],
   ["Smokey","Toast","Pacha","Mau"]
 ]
+
+seating_plan = {}
+seats.each_with_index { |row, index|
+  seating_plan[row] = index
+}
+
+seating_plan.each do |seats, row|
+  puts "#{seats}, #{row + 1}"
+end
