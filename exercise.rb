92b08681e7a81023b03afbc103854d8bc8487936
Zach Job <zachery.job@gmail.com>
2017-10-20 13:53:37 -0400

    replace repetitive code; remove extra hash

--- exercise.rb
+++ exercise.rb
@@ -5,13 +5,7 @@ seats = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
-seating_plan = {}
-
 seats.each_with_index {|row, index|
-  seating_plan[row] = index
-}
-
-seating_plan.each { |row, index|
   row.each_with_index { |seat, place|
     if seat == nil
       puts "Row #{index + 1} seat #{place + 1} is free."
