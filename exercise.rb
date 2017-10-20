b533cc0773ea158124b4d4186cde7acbf4dfa6af
Zach Job <zachery.job@gmail.com>
2017-10-20 14:00:10 -0400

    Complete exercise

--- exercise.rb
+++ exercise.rb
@@ -8,7 +8,15 @@ seats = [
 seats.each_with_index {|row, index|
   row.each_with_index { |seat, place|
     if seat == nil
-      puts "Row #{index + 1} seat #{place + 1} is free."
+      puts "Row #{index + 1} seat #{place + 1} is free. Do you want to sit here? (y/n)."
+      answer = gets.chomp
+      if answer == "y"
+        puts "Sweet! What's your name?"
+        seat = gets
+        seats[index][place] = seat
+      end
     end
   }
 }
+
+puts seats
