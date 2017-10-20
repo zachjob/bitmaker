94a8c587497a34aac9165d4ae13d77abafd44201
Zach Job <zachery.job@gmail.com>
2017-10-20 15:45:12 -0400

    Create if statement for various scenarios

---
+++ exercise.rb
@@ -0,0 +1,10 @@
+# if the number is divisible by 2 and 7 the value should be the key multiplied by 2
+if num % 2 == 0 && num % 7 == 0
+# if the number is divisible by 2 the value should be one more than the key
+elsif num % 2 == 0
+# if the number is divisible by 7 the value should be one less than the key
+elsif num % 7 == 0
+# otherwise the value should be the same number as the key
+else
+
+end
