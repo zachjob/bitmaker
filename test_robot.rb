6f572dc7d538254038ec71da1aadb1dd5e839c64
Zach Job <zachery.job@gmail.com>
2017-10-26 15:09:58 -0400

    Complete tests

--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,88 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.needs_repairs = true
+    robo.foreign_model = true
     # act
-
+    robo.station
     # assert
+    assert_equal(1, robo.station)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.needs_repairs = true
+    robo.vintage_model = true
     # act
-
+    robo.station
     # assert
+    assert_equal(2, robo.station)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.needs_repairs = true
     # act
-
+    robo.station
     # assert
+    assert_equal(3, robo.station)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.needs_repairs = false
     # act
-
+    robo.station
     # assert
+    assert_equal(4, robo.station)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.todos = []
     # act
-
+    robo.prioritize_tasks
     # assert
+    assert_equal(-1, robo.prioritize_tasks)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.todos = ["Go to the store.", "Buy milk.",]
     # act
-
+    robo.prioritize_tasks
     # assert
+    expected_result = robo.todos.max
+    assert_equal(expected_result, robo.prioritize_tasks)
   end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.day_off = "Wednesday"
     # act
-
+    robo.workday?("Wednesday")
     # assert
+    actual_result = robo.workday?("Wednesday")
+    assert_equal(false, actual_result)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
-
+    robo = Robot.new
+    robo.day_off = "Wednesday"
     # act
-
+    robo.workday?("Thursday")
     # assert
+    actual_result = robo.workday?("Thursday")
+    assert_equal(true, actual_result)
   end
 
 end
