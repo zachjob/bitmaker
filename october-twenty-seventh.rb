ca8da89649fd8adedc531d074cacbde32ed21d00
Zach Job <zachery.job@gmail.com>
2017-10-27 12:11:49 -0400

    Add each person's placements to their scores

--- october-twenty-seventh.rb
+++ october-twenty-seventh.rb
@@ -4,12 +4,14 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
 {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
 {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
 {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+
 score = {'Smudge' => 0, 'Tigger' => 0, 'Simba' => 0, 'Bella' => 0, 'Lucky' => 0, 'Felix' => 0, 'Boots' => 0}
 
 ballots.each do |b|
   b.each do |key, value|
-    puts "#{key}: #{value}"
-    puts "----------"
+    score[value] += key
   end
 
 end
+
+puts score
