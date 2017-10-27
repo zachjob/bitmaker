ae4fa8ee2d0afe78ae98a70c153637ee72bcbb47
Zach Job <zachery.job@gmail.com>
2017-10-27 12:09:45 -0400

    Create scores hash to record each person's score in

--- october-twenty-seventh.rb
+++ october-twenty-seventh.rb
@@ -1,9 +1,10 @@
 ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
-           {1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
-           {1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
-           {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
-           {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
-           {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+{1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
+{1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
+{1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
+{1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
+{1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+score = {'Smudge' => 0, 'Tigger' => 0, 'Simba' => 0, 'Bella' => 0, 'Lucky' => 0, 'Felix' => 0, 'Boots' => 0}
 
 ballots.each do |b|
   b.each do |key, value|
