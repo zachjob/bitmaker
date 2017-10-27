9690361bfcce2985ecf40c18fff213a0b9199aed
Zach Job <zachery.job@gmail.com>
2017-10-27 15:06:27 -0400

    Validate project and rewards models to check for missing

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,5 @@ class Project < ActiveRecord::Base
   has_many :users, through: :pledges # backers
   belongs_to :user # project owner
 
-  validates :title, :description, :goal, :start_date, :end_date, presence: true
+  validates :title, :description, :goal, :start_date, :end_date, :user_id, presence: true
 end
