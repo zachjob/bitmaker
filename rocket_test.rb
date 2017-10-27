b09612ee64b8b434a23ccbafbadc33c2b3d6ca08
Zach Job <zachery.job@gmail.com>
2017-10-27 13:13:52 -0400

    Add setup and teardown methods.

--- rocket_test.rb
+++ rocket_test.rb
@@ -1,6 +1,20 @@
 require "minitest/autorun"
+require "minitest/pride"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def teardown
+    @rocket = nil
+  end
+
+  def test_less_than_zero_returns_true
+    skip
+    result =
+    expected =
+    assert_equal(expected, result)
+  end
 end
