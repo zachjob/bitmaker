d31573557ef4d4a15958ca3ddbde9e256ab969f9
Zach Job <zachery.job@gmail.com>
2017-10-27 11:58:02 -0400

    Add provided data

---
+++ october-twenty-seventh.rb
@@ -0,0 +1,6 @@
+ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
+           {1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
+           {1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
+           {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
