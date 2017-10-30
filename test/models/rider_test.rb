8f6975995f8e23a5c4ffe517746edaaf6044fd3d
Zach Job <zachery.job@gmail.com>
2017-10-30 16:37:44 -0400

    Complete rider tests

--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,8 +5,8 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
-    aegon = nil
-    daenerys = nil
+    aegon = create(:rider, name: 'Aegon', sword: 'Blackfyre')
+    daenerys = create(:rider, name: 'Daenerys', sword: 'Lightbringer')
 
     # can't change the assert_ statements!
     skip unless aegon && daenerys
