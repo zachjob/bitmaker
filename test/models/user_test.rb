2f5f557b137dac794af95a7375784dd10386399d
Lan Phan <lanc_phan@hotmail.com>
2017-10-30 15:59:07 -0400

    test the total sum of pledges

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,5 +30,34 @@ class UserTest < ActiveSupport::TestCase
     assert user.name == "Lan Phan", "User name doesn't contain first and last name"
   end
 
+  test 'total_pledged' do
+    userA = User.create!(first_name: "Lan", last_name: "Phan", email: "lan@email.com", password: "password", password_confirmation: "password")
+
+    userB = User.create!(first_name: "Patrick", last_name: "Stewart", email: "patrick@email.com", password: "password", password_confirmation: "password")
+
+    project1 = Project.create!(
+      title:       'Anti-cut backpack',
+      description: 'prevent burgulars from cutting your backpack',
+      start_date:  Date.tomorrow,
+      end_date:    Date.tomorrow + 2.month,
+      goal:        30000,
+      user:        userA
+    )
+
+    pledge = Pledge.create!(
+      dollar_amount:  50.00,
+      project:        project1,
+      user:           userB
+    )
+
+    pledge = Pledge.create!(
+      dollar_amount:  50.00,
+      project:        project1,
+      user:           userB
+    )
+
+    assert_equal(userB.total_pledged, 100.00)
+    assert_equal(userA.total_pledged, 0)
+  end
 
 end
