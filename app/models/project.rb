635b7239b6bb70dd9310846117d24e2dfbbb818d
Zach Job <zachery.job@gmail.com>
2017-10-31 15:00:54 -0400

    Display total raised on project page

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,21 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :end_date, :user_id, presence: true
+
+  def earned
+    pledges.sum(&:dollar_amount)
+  end
+
+  # def earned
+  #   pledges.sum do |pledge|
+  #     pledge.dollar_amount
+  #   end
+  # end
+  # def earned
+  #   total = 0
+  #   pledges.each do |pledge|
+  #     total += pledge.dollar_amount
+  #   end
+  #   total
+  # end
 end
