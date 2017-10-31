6d795e1f9799a1362cfa869ab6dac6fd299141d0
Lan Phan <lanc_phan@hotmail.com>
2017-10-31 11:48:16 -0400

    error messages in the user's form and some slight change in other files

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,24 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :end_date, :user_id, presence: true
+
+  def start_date_cannot_be_in_the_past
+    if start_date <= Date.today
+      errors.add(:start_date, 'Start date cannot be in the future')
+    end
+  end
+
+  def end_date_later_than_start_date
+    if end_date < start_date && end_date.present?
+      errors.add(:end_date, 'Must be later than start date')
+    end
+  end
+
+    def total_pledged_in_project
+    self.pledges.sum(:dollar_amount)
+  end
+
+  def backed_up?(current_user)
+    return self.users.include?(current_user)
+  end
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,7 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
+  has_many :pledges
+
   validates :dollar_amount, :description, presence: true
+  validates :dollar_amount, numericality: {greater_than: 0}
 end
