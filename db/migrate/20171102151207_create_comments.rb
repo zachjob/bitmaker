d06c3b34e91ec798c8fe1322b2eeeacff3c82089
Lan Phan <lanc_phan@hotmail.com>
2017-11-02 11:16:58 -0400

    generate a CreateComments migration with project_id, user_id and comment. Plus, rails db:migrate

--- db/migrate/20171102151207_create_comments.rb
+++ db/migrate/20171102151207_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
+      t.text :comment
+      t.integer :project_id
+      t.integer :user_id
 
       t.timestamps
     end
