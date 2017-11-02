0adac80358bdc380eecf377e96d1602077073f5b
Lan Phan <lanc_phan@hotmail.com>
2017-11-02 11:33:05 -0400

    edit the create method and made the destroy method in the Comments Controller and create the comments'view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,25 @@ class CommentsController < ApplicationController
   def create
     @project = Project.find(params[:project_id])
     @comment = Comment.new
-  end
+    @comment.user = current_user
+    @comment.project = @project
+    @comment.comment = params[:comment][:comment]
 
-  def update
+    if @comment.save
+      flash[:notice] = "Thank you for submitting your comment!"
+      redirect_to project_url(@project)
+    else
+      flash[:alert] = "Sorry, there was a problem with submitting your comment!"
+      render "projects/show"
+    end
+
   end
 
   def destroy
+    @project = Project.find(params[:project_id])
+    @comment = @project.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Your comment has been successfully deleted!"
+    redirect_to project_url(@project)
   end
 end
