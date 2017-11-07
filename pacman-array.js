5b74910a9ece9170ce966ae030da0ae23de881d6
Zach Job <zachery.job@gmail.com>
2017-11-07 14:01:01 -0500

    Rename pacman-array to just pacman and comment out unused code

--- pacman-array.js
+++
@@ -1,8 +0,0 @@
-var ghosts = ["Inky", "Blinky", "Pinky", "Clyde"];
-
-console.log("There are " + ghosts.length + " ghosts.");
-console.log("Their names are: ");
-console.log(ghosts[0]);
-console.log(ghosts[1]);
-console.log(ghosts[2]);
-console.log(ghosts[3]);
---
+++ pacman.js
@@ -0,0 +1,9 @@
+// var ghosts = ["Inky", "Blinky", "Pinky", "Clyde"];
+
+// console.log("There are " + ghosts.length + " ghosts.");
+// console.log("Their names are: ");
+// console.log(ghosts[0]);
+// console.log(ghosts[1]);
+// console.log(ghosts[2]);
+// console.log(ghosts[3]);
+
