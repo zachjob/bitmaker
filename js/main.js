923f7adceee0c082571ac744176f76c9b0ef10ea
Zach Job <zachery.job@gmail.com>
2017-11-13 15:11:54 -0500

    Add error class to the input fields when the submit button is clicked.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log('Your DOM is ready!')
+
     var modal = document.querySelector(".modal");
     var signin = document.querySelector(".signin");
     var close = document.querySelector(".close");
+    var submit = document.querySelector(".submit");
+    var username = document.querySelector('#user');
+    var password = document.querySelector("#pass");
 
     signin.addEventListener("click", function(e) {
         modal.style.display = 'block';
@@ -10,5 +14,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     close.addEventListener("click", function(e) {
       modal.style.display = "none";
+    });
+
+    submit.addEventListener("click", function(e) {
+      username.classList.add('error');
+      password.classList.add('error')
     });
 });
