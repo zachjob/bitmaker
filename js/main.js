9cb1d05e6cedfb60ec6f46b7ce6bed257389977f
Zach Job <zachery.job@gmail.com>
2017-11-13 15:07:58 -0500

    Close modal when close button is clicked.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     signin.addEventListener("click", function(e) {
         modal.style.display = 'block';
-//     if (control.classList.contains("on")) {
-//       control.classList.remove("on");
-//       control.classList.add("off");
+    });
 
-//       status.innerText = "Hey, who turned off the lights?";
-
-//       body.classList.remove("light");
-//       body.classList.add("dark");
-//     } else {
-//       control.classList.remove("off");
-//       control.classList.add("on");
-
-//       status.innerText = "It's so bright in here!";
-
-//       body.classList.remove("dark");
-//       body.classList.add("light");
-//     }
+    close.addEventListener("click", function(e) {
+      modal.style.display = "none";
     });
 });
