9a7dbc3303c925d787e9e01317a2a092c9b94763
Zach Job <zachery.job@gmail.com>
2017-11-13 14:37:01 -0500

    Control the page background using a lightswitch.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
 
+    var body = document.querySelector('.light');
+    var control = document.querySelector('.switch');
 
+    control.addEventListener("click", function( e ) {
+        
+        if (control.classList.contains('on')) {
+            control.classList.remove('on');
+            control.classList.add("off");
+
+            body.classList.remove('light');
+            body.classList.add('dark');
+        } else {
+            control.classList.remove("off");
+            control.classList.add("on");
+
+            body.classList.remove("dark");
+            body.classList.add("light");
+        };
+        
+  });
+});
