b02b680ef61a84329a0ce91d9c6e4e9d21a25ddd
Zach Job <zachery.job@gmail.com>
2017-11-13 14:46:00 -0500

    Answer part 1.

---
+++ draw.rb
@@ -0,0 +1,17 @@
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+answer = {rows: 4, cols: 4, char: '*'}
+
+puts draw_shape(answer)
