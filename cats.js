388a0be10252e98a9701ec08fb40743300d6c4d6
Zach Job <zachery.job@gmail.com>
2017-11-15 15:52:43 -0500

    Complete assignment.

--- cats.js
+++ cats.js
@@ -8,7 +8,9 @@ $(function() {
         dataType: "JSON",
     }).done(function(data) {
       console.log(data);
-      $("#cat1").append("<img src=\"" + data.cats[0].photo + "\"/>");
+      $("#cat1").append('<img src="' + data.cats[0].photo + '" alt="Photo of ' + data.cats[0].name + '"/>');
+      $("#cat2").append('<img src="' + data.cats[1].photo + '" alt="Photo of ' + data.cats[1].name + '"/>');
+      $("#cat3").append('<img src="' + data.cats[2].photo + '" alt="Photo of ' + data.cats[2].name + '"/>');
     });
   });
 });
