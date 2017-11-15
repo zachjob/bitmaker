a3d2ef837caf7fe9a9638fdeb5ceeace7c96982c
Zach Job <zachery.job@gmail.com>
2017-11-15 14:46:28 -0500

    Request election results through AJAX.

--- election.js
+++ election.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  // Imagination!
-
+  console.log("Your DOM is ready.");
+  
+  $.ajax({ 
+    url: "https://bb-election-api.herokuapp.com/", 
+    method: "GET",
+    dataType: "json",
+  }).done(function(data) {
+    for (var i = data.length; i >= 0; i--) {
+      
+    };
+  });
 });
