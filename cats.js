7d02c7539df9241c0df46ff8539c59148dbdd60f
Zach Job <zachery.job@gmail.com>
2017-11-15 15:38:38 -0500

    Return JSON Data when summon cats button is clicked.

--- cats.js
+++ cats.js
@@ -1,6 +1,13 @@
 $(function() {
   console.log("ready!");
   $(".summon-cats").click(function() {
-    console.log("Summoning cats...");
+    // console.log("Summoning cats...");
+    $.ajax({ 
+        url: "http://bitkittens.herokuapp.com/cats.json",
+        method: "GET",
+        dataType: "JSON",
+    }).done(function(data) {
+      console.log(data);
+    });
   });
 });
